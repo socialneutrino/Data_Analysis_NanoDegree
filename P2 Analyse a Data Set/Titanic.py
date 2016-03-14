20ef55a59f7058a6db5b32198220d2af42fc2af3
socialneutrino <advspiers@gmail.com>
2016-03-14 22:13:51 +0000

    added P2 analyse Titanic

---
+++ P2 Analyse a Data Set/Titanic.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 17 16:11:35 2016
+
+@author: Alex
+"""
+
+import numpy as np
+import pandas as pd
+
+titanic = pd.read_csv('titanic_data.csv')
+
+print(titanic)
